be8ca60d56780805c0d3cd941741ddeb708fc03e
Chris Maltby <chris.maltby@gmail.com>
2020-10-07 19:12:43 +0100

    Remove unneeded test code

--- src/main.c
+++ src/main.c
@@ -31,27 +31,6 @@ void TestFn(UINT16 index) {
 
 int main()
 {
-  UBYTE i;
-  // Init LCD
-  LCDC_REG = 0x67;
-
-  // Position Window Layer
-  WX_REG = 7;
-  WY_REG = MAXWNDPOSY + 1U;
-
   TestFn(0);
-
-  PUSH_BANK(image_bank);
-
-  for (i = 0; i != 18; i++)
-  {
-    set_bkg_tiles(0, i, 21, 1, image_ptr);
-    image_ptr += image_tile_width;
-  }
-
-  POP_BANK;
-
-  DISPLAY_ON;
-
   return 0;
 }

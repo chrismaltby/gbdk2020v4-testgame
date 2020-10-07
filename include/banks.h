a9d0643c6fd5c6b138607055013f9cabf85c8c86
Chris Maltby <chris.maltby@gmail.com>
2020-10-07 19:40:29 +0100

    Single file bug

--- include/banks.h
+++
@@ -1,7 +0,0 @@
-#ifndef BANKS_H
-#define BANKS_H
-
-extern const unsigned char bank_6_data[];
-extern const unsigned char bank_7_data[];
-
-#endif
--- src/main.c
+++ src/main.c
@@ -1,42 +1,54 @@
 #include <gb/gb.h>
+#include <stdio.h>
 
-UBYTE *test1;
+UWORD test1;
 UBYTE test2;
-UBYTE collision_bank;
+UBYTE test3;
 
-typedef struct _BankPtr {
+typedef struct _BankPtr
+{
   UBYTE bank;
   UWORD offset;
 } BankPtr;
 
-const BankPtr scene_bank_ptrs1[] = {
-{0x06,0x3C53},{0x06,0x3DBB}
-};
+const BankPtr scene_bank_ptrs[] = {
+    {0x06, 0x3C53}};
 
-const BankPtr collision_bank_ptrs1[] = {
-{0x06,0x2D43},{0x06,0x3983}
-};
-
-void TestFn2(UBYTE i) {
+void TestFn2(UBYTE i)
+{
   test2 = i;
 }
 
-void TestFn(UINT16 index) {
+void TestFn(UINT16 index)
+{
   // static UBYTE bank;
   UBYTE bank;
-  UBYTE* data_ptr;
+  UWORD data_ptr;
 
-  bank = scene_bank_ptrs1[index].bank;
-  data_ptr = (scene_bank_ptrs1[index].offset + 0x4000);
-  collision_bank = collision_bank_ptrs1[index].bank;
+  bank = scene_bank_ptrs[index].bank;
+  data_ptr = scene_bank_ptrs[index].offset;
 
+  /* This section doesn't matter but bug doesn't trigger without it */
+  test3 = 1;
   TestFn2(bank);
   test1 = data_ptr;
-}
+  /**/
 
+  if (data_ptr == 0x3C53) {
+    printf("OK\n");
+  } else {
+    printf("FAIL\n");
+  }
+}
 
 int main()
 {
+  LCDC_REG = 0x67;
+  BGP_REG = OBP0_REG = OBP1_REG = 0xE4U;
+  WX_REG = MAXWNDPOSX;
+  WY_REG = MAXWNDPOSY;
+
   TestFn(0);
+
   return 0;
 }
